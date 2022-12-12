38d0e6d1265df0b98c0619fa6faf53b208bda7f4
Thiago Mota Santos <lucimara1422@gmail.com>
2022-12-12 16:38:10 -0300

    seo

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -6,7 +6,6 @@ import RightContent from "../../components/RightContent";
 import { Container, Wrapper } from "../../styles/slugstyle";
 import { client, ssrCache } from "../../graphql/api";
 import { Page } from "../../components/pageSEO";
-import ImageSeo from "../../components/imgSeo";
 
 export default function Post({ slug }) {
   const [
@@ -20,35 +19,33 @@ export default function Post({ slug }) {
   });
 
   return (
-    <ImageSeo imgSEO={post.seo.image.url.toString()}>
-      <Page
-        title={post.title}
-        description={post.seo.description}
-        path={`blog/${slug}`}
-        openGraph={{
-          images: [
-            {
-              url: `${post.seo.image.url.toString()}`,
-            },
-          ],
-        }}
-      >
-        <Container>
-          <Wrapper>
-            <Blog
-              title={post.title}
-              name={"Th"}
-              // date={post.date}
-              date={"03 Out, 2022"}
-              image={post.coverImage.url}
-              previewContent={null}
-              textContent={post.content.markdown}
-            />
-            <RightContent />
-          </Wrapper>
-        </Container>
-      </Page>
-    </ImageSeo>
+    <Page
+      title={post.title}
+      description={post.seo.description}
+      path={`blog/${slug}`}
+      openGraph={{
+        images: [
+          {
+            url: `${post.seo.image.url.toString()}`,
+          },
+        ],
+      }}
+    >
+      <Container>
+        <Wrapper>
+          <Blog
+            title={post.title}
+            name={"Th"}
+            // date={post.date}
+            date={"03 Out, 2022"}
+            image={post.coverImage.url}
+            previewContent={null}
+            textContent={post.content.markdown}
+          />
+          <RightContent />
+        </Wrapper>
+      </Container>
+    </Page>
   );
 }
 
