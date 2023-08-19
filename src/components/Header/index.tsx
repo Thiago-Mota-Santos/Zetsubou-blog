cb8cdd0ddecdbe14fac9e8f91a820e1a577acb39
Thiago <lucimara1422@gmail.com>
2023-08-19 19:32:27 -0300

    feat(Header) refactor

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,52 +1,25 @@
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import { Container, MenuIcon, MobileIcon, NavMenu, XIcon } from "./styles";
+import { HeaderMenu, Wrapper, NavMenu, LogoContainer, LogoImage } from "./styles";
 
 const Header: React.FC = () => {
-  const [click, setClick] = useState(false);
-
-  const [button, setButton] = useState(true);
-
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
-
-  useEffect(() => {
-    showButton();
-  }, []);
-
-  if (typeof window !== "undefined") {
-    window.addEventListener("resize", showButton);
-  }
-
-  const handleClick = () => {
-    setClick(!click);
-  };
-
   return (
-    <Container>
-      <MobileIcon onClick={handleClick}>
-        {click ? <MenuIcon /> : <XIcon />}
-      </MobileIcon>
-
-      <NavMenu click={click}>
-        <li>
-          <Link href={"/"}>
-            <a>home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href={"/blog"}>
-            <a>posts</a>
-          </Link>
-        </li>
-      </NavMenu>
-    </Container>
+    <HeaderMenu>
+  <Wrapper>
+  <LogoContainer>
+      <Link href="/">
+      <LogoImage src="https://media.graphassets.com/xU10WhwGTTyjI7tlTwfr" width={28} height={28} alt="Zetsubou Blog logo"/>
+      </Link>
+      <Link href="/">
+        Zetsubou Blog
+      </Link>
+    </LogoContainer>
+    <NavMenu>
+      <Link href="/blog">Posts</Link>
+    </NavMenu>
+  </Wrapper>
+</HeaderMenu>
   );
 };
 
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,89 +1,70 @@
-import { FaBars, FaTimes } from 'react-icons/fa';
-import styled, {css} from 'styled-components';
+import styled from 'styled-components';
+import Image from 'next/image'
+import Link from 'next/link'
 
-
-export const Container = styled.div`
-    display: flex;
-    width: 100%;
-    position: sticky;
-    z-index: 2;
-    box-shadow: rgb(0 0 0 / 10%) 0px 1px 0px;
-    top: 0;
-    height: 54px;
-    align-items: center;
-    padding: 11px 20px;
-   
-    background: var(--header-dark);
+export const HeaderMenu = styled.header`
+  color: #ccc;
+  background-color: #111;
+  font-family: sans-serif;
 `;
 
-export const NavMenu = styled.ul`
-    display:flex;
-    width: 100%;
-    margin-left: -20px;
 
-    @media screen and (max-width: 960px){
-      display: flex;
-      flex-direction: column;
-      opacity: ${({ click }) => (click ? '1' : '0')};
-      transition: all 0.8s ease;
-      width: 100%;
-      height: 30vh;
-      position: absolute;
-      top: 50px;
-      background: var(--header-dark);
-    }
-    
-    >li{
-        margin-left: 25px;
-        font-size: 14px;
-        cursor:pointer;
-        margin-top: 5px;
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem;
+`;
 
-        > a{
-        color:var(--header-item); 
-        text-decoration: none;
-
-        &:hover{
-            color:var(--header-item-hover);
-            transition: color 0.5s ease-out;
-
-          
-        }
-      }   
-    }
-    
+export const NavMenu = styled.nav`
+  display: flex;
+  align-items: center;
   
-`;
-
-const IconCss = css`
-  fill: var(--header-item);
-  width: 19px;
-  height: 19px;
-`;
-
-export const MenuIcon = styled(FaTimes)`
-  ${IconCss};
-
-`
-
-export const XIcon = styled(FaBars)`
-  ${IconCss};
-`;
-
-
-
-export const MobileIcon = styled.div`
-  display: none;
-
-  @media screen and (max-width: 960px){
-    display: block;
-    padding: 5px;
-    position: absolute;
-    top: 0;
-    right: 0;
-    transform: translate(-100%, 60%);
-    /* font-size: 1.8rem; */
-    cursor: pointer;
+  a {
+    margin-right: 1rem;
+    color: #ccc;
+    text-decoration: none;
     
+    &:hover {
+      color: white;
+    }
+  }
+ 
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  > a {
+    margin-left: 0.5rem;
+    color: #ccc;
+    text-decoration: none;
+
+
+    &:hover{
+      color: white
+    }
+
   }
 `;
+
+
+export const  LogoImage = styled(Image)`
+  color: white;
+  align-items: center;
+
+
+  a {
+    margin-left: 1rem;
+    color: #ccc;
+    text-decoration: none;
+
+    &:hover {
+      color: white;
+    }
+  }
+
+`;
+
+
